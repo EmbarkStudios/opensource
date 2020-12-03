0f841a796e2cf200c219b99dcddcf0f7ba61c02c
Louis Pilfold <louis@lpil.uk>
2020-12-03 18:08:17 +0000

    Check maintainers

---
+++ tools/embark-oss/src/check_maintainers.rs
@@ -0,0 +1,135 @@
+use crate::codeowners::CodeOwners;
+use eyre::{eyre, WrapErr};
+use itertools::Itertools;
+use std::collections::HashSet;
+
+const OPENSOURCE_WEBSITE_DATA_URL: &str =
+    "https://raw.githubusercontent.com/EmbarkStudios/opensource-website/main/data.json";
+
+#[derive(Debug)]
+enum Error {
+    NoPrimaryMaintainer,
+    NoCodeOwnersFile,
+    UnknownError(eyre::Error),
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::NoPrimaryMaintainer => {
+                write!(f, "No maintainers were found for * the CODEOWNERS file.")
+            }
+            Self::NoCodeOwnersFile => {
+                write!(f, "No CODEOWNERS file could be read for this project.")
+            }
+            Self::UnknownError(error) => error.fmt(f),
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
+}
+
+#[derive(Debug)]
+struct Project {
+    name: String,
+    status: Result<HashSet<String>, Error>,
+}
+
+pub async fn main() -> eyre::Result<()> {
+    // Download list of projects and download CODEOWNERS file for each one
+    let projects = download_projects_list().await?;
+    let futures = projects.into_iter().map(lookup_project);
+    let projects = futures::future::join_all(futures).await;
+
+    // Print results
+    projects.iter().for_each(print_status);
+
+    if projects.iter().any(|p| p.status.is_err()) {
+        std::process::exit(1);
+    };
+    Ok(())
+}
+
+fn print_status(Project { name, status }: &Project) {
+    match status {
+        Ok(maintainers) => println!("✔️  {} ({})", name, maintainers.iter().join(", ")),
+        Err(error) => {
+            println!("❌ {}", name);
+            println!("   {}", error);
+        }
+    }
+}
+
+async fn download_projects_list() -> eyre::Result<Vec<OpenSourceWebsiteProject>> {
+    let response = reqwest::get(OPENSOURCE_WEBSITE_DATA_URL)
+        .await
+        .wrap_err("Failed to download Embark open source website data.json")?;
+
+    if response.status() != 200 {
+        return Err(eyre!("Expected status code 200, got {}", response.status()))
+            .wrap_err("Unable to download Embark open source website data.json");
+    }
+
+    Ok(response
+        .json::<OpenSourceWebsiteData>()
+        .await
+        .wrap_err("Failed to parse OSS website data.json")?
+        .projects)
+}
+
+async fn lookup_project(project: OpenSourceWebsiteProject) -> Project {
+    let name = project.name;
+    Project {
+        status: lookup_project_status(&name).await,
+        name,
+    }
+}
+
+async fn lookup_project_status(name: &str) -> Result<HashSet<String>, Error> {
+    let url = format!(
+        "https://raw.githubusercontent.com/EmbarkStudios/{}/main/.github/CODEOWNERS",
+        name
+    );
+    // Download the CODEOWNERS file
+    let response = reqwest::get(&url)
+        .await
+        .wrap_err(format!("Failed to download CODEOWNERS for {}", name))
+        .map_err(Error::UnknownError)?;
+
+    // Ensure the CODEOWNERS file was successfully downloaded
+    if response.status() == 404 {
+        return Err(Error::NoCodeOwnersFile);
+    }
+    if response.status() != 200 {
+        return Err(eyre!("Expected status code 200, got {}", response.status()))
+            .wrap_err("Unable to download Embark open source website data.json")
+            .map_err(Error::UnknownError)?;
+    }
+
+    // Parse the body to get the CODEOWNERS file contents
+    let text = response
+        .text()
+        .await
+        .wrap_err(format!("Failed to get CODEOWNERS text body for {}", name))
+        .map_err(Error::UnknownError)?;
+
+    // Determine if there is at least 1 primary maintainer listed for each project
+    CodeOwners::new(&text)
+        .primary_maintainers()
+        .cloned()
+        .ok_or(Error::NoPrimaryMaintainer)
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct OpenSourceWebsiteData {
+    projects: Vec<OpenSourceWebsiteProject>,
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct OpenSourceWebsiteProject {
+    name: String,
+}
--- tools/embark-oss/src/main.rs
+++ tools/embark-oss/src/main.rs
@@ -1,5 +1,61 @@
+#![deny(warnings)]
+#![warn(
+    clippy::all,
+    clippy::doc_markdown,
+    clippy::dbg_macro,
+    clippy::todo,
+    clippy::empty_enum,
+    clippy::enum_glob_use,
+    clippy::mem_forget,
+    clippy::use_self,
+    clippy::filter_map_next,
+    clippy::needless_continue,
+    clippy::needless_borrow,
+    clippy::match_wildcard_for_single_variants,
+    clippy::if_let_mutex,
+    clippy::mismatched_target_os,
+    clippy::await_holding_lock,
+    clippy::match_on_vec_items,
+    clippy::imprecise_flops,
+    clippy::suboptimal_flops,
+    clippy::lossy_float_literal,
+    clippy::rest_pat_in_fully_bound_structs,
+    clippy::fn_params_excessive_bools,
+    clippy::inefficient_to_string,
+    clippy::linkedlist,
+    clippy::macro_use_imports,
+    clippy::option_option,
+    clippy::verbose_file_reads,
+    clippy::unnested_or_patterns,
+    rust_2018_idioms,
+    future_incompatible,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unstable_features,
+    nonstandard_style,
+    unused_import_braces,
+    unused_qualifications,
+    unused_results
+)]
+
+mod check_maintainers;
 mod codeowners;
 
-fn main() {
-    println!("Hello, world!");
+use structopt::clap::AppSettings;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(global_settings = &[AppSettings::ColoredHelp, AppSettings::VersionlessSubcommands])]
+enum Command {
+    #[structopt(about = "Check all projects have a maintainer at Embark")]
+    CheckMaintainers {},
+}
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    match Command::from_args() {
+        Command::CheckMaintainers {} => check_maintainers::main().await,
+    }
 }
