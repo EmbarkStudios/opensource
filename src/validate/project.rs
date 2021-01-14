60bf1588e955449395ba718f76f5bcc670285e2d
Louis Pilfold <louis@lpil.uk>
2021-01-14 15:05:01 +0000

    Convert validation functions to Project methods

--- src/validate/project.rs
+++ src/validate/project.rs
@@ -27,13 +27,9 @@ impl Project {
     }
 
     pub async fn validate(self, context: &Context) -> Self {
-        let maintainers =
-            lookup_project_maintainers(&self.name, &context.embark_github_organisation_members)
-                .await;
-
-        let rust_ecosystem_registration = check_rust_ecosystem_registration(&self.name, context);
-
-        let archive_status = check_archive_status(&self.name, context);
+        let maintainers = self.lookup_project_maintainers(context).await;
+        let rust_ecosystem_registration = self.check_rust_ecosystem_registration(context);
+        let archive_status = self.check_archive_status(context);
 
         Self {
             name: self.name,
@@ -76,63 +72,62 @@ impl Project {
                 .join("\n"),
         )
     }
+
+    async fn lookup_project_maintainers(&self, context: &Context) -> eyre::Result<HashSet<String>> {
+        // Download CODEOWNERS from one of the accepted branches
+        let get = |branch| {
+            github::download_repo_file("EmbarkStudios", &self.name, branch, ".github/CODEOWNERS")
+        };
+        let text = get("main")
+            .or_else(|_| get("master"))
+            .await
+            .wrap_err("Unable to determine maintainers")?;
+
+        // Determine if there is at least 1 primary maintainer listed for each project
+        let maintainers = github::CodeOwners::new(&text)
+            .wrap_err("Unable to determine maintainers")?
+            .primary_maintainers()
+            .cloned()
+            .ok_or_else(|| eyre!("No maintainers were found for * the CODEOWNERS file"))?;
+
+        // Ensure all maintainers are in the EmbarkStudios organisation
+        let mut maintainers_not_in_embark = maintainers
+            .difference(&context.embark_github_organisation_members)
+            .peekable();
+        if maintainers_not_in_embark.peek().is_some() {
+            return Err(eyre!(
+                "Maintainers not public EmbarkStudios members: {}",
+                maintainers_not_in_embark.join(", "),
+            ));
+        }
+
+        Ok(maintainers)
+    }
+
+    fn check_rust_ecosystem_registration(&self, context: &Context) -> eyre::Result<()> {
+        let tags = match context.opensource_website_projects.get(&self.name) {
+            Some(project) => &project.tags,
+            None => return Ok(()),
+        };
+        if tags.contains("rust") && !context.rust_ecosystem_readme.contains(&self.name) {
+            Err(eyre!("Rust project not in the rust-ecosystem README"))
+        } else {
+            Ok(())
+        }
+    }
+
+    fn check_archive_status(&self, context: &Context) -> eyre::Result<()> {
+        let repo = context.embark_github_repos.get(&self.name).ok_or_else(|| {
+            eyre!("Unable to find project in the EmbarkStudios GitHub organisation")
+        })?;
+        if repo.archived {
+            Err(eyre!("Project has been archived on GitHub"))
+        } else {
+            Ok(())
+        }
+    }
 }
 
 fn not_yet_checked<T>() -> eyre::Result<T> {
     Err(eyre!("This property has not yet been validated"))
-}
-
-async fn lookup_project_maintainers(
-    name: &str,
-    members: &HashSet<String>,
-) -> eyre::Result<HashSet<String>> {
-    // Download CODEOWNERS from one of the accepted branches
-    let get =
-        |branch| github::download_repo_file("EmbarkStudios", name, branch, ".github/CODEOWNERS");
-    let text = get("main")
-        .or_else(|_| get("master"))
-        .await
-        .wrap_err("Unable to determine maintainers")?;
-
-    // Determine if there is at least 1 primary maintainer listed for each project
-    let maintainers = github::CodeOwners::new(&text)
-        .wrap_err("Unable to determine maintainers")?
-        .primary_maintainers()
-        .cloned()
-        .ok_or_else(|| eyre!("No maintainers were found for * the CODEOWNERS file"))?;
-
-    // Ensure all maintainers are in the EmbarkStudios organisation
-    let mut maintainers_not_in_embark = maintainers.difference(members).peekable();
-    if maintainers_not_in_embark.peek().is_some() {
-        return Err(eyre!(
-            "Maintainers not public EmbarkStudios members: {}",
-            maintainers_not_in_embark.join(", "),
-        ));
-    }
-
-    Ok(maintainers)
-}
-
-fn check_rust_ecosystem_registration(name: &str, context: &Context) -> eyre::Result<()> {
-    let tags = match context.opensource_website_projects.get(name) {
-        Some(project) => &project.tags,
-        None => return Ok(()),
-    };
-    if tags.contains("rust") && !context.rust_ecosystem_readme.contains(name) {
-        Err(eyre!("Rust project not in the rust-ecosystem README"))
-    } else {
-        Ok(())
-    }
-}
-
-fn check_archive_status(name: &str, context: &Context) -> eyre::Result<()> {
-    let repo = context
-        .embark_github_repos
-        .get(name)
-        .ok_or_else(|| eyre!("Unable to find project in the EmbarkStudios GitHub organisation"))?;
-    if repo.archived {
-        Err(eyre!("Project has been archived on GitHub"))
-    } else {
-        Ok(())
-    }
 }
