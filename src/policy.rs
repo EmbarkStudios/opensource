43fc0ab1b895304f4d34ce36434b64f8d70495a0
Tom Solberg <tom.solberg@embark-studios.com>
2022-05-03 14:44:00 +0200

    Add `keith` as non-embark maintainer (#51)
    Add `keith` as non-embark maintainer
    
    See https://github.com/EmbarkStudios/k8s-buildkite-plugin/issues/50

--- src/policy.rs
+++ src/policy.rs
@@ -2,11 +2,14 @@
 
 /// Generally we require that all maintainers of Embark Studios open source projects are
 /// part of the Embark org, but this list allows some explicit exceptions
-pub const ALLOWED_NON_EMBARK_MAINTAINERS: [&str; 1] = [
+pub const ALLOWED_NON_EMBARK_MAINTAINERS: [&str; 2] = [
     // Emil (https://github.com/emilk) worked at Embark and built 2 open source crates that he continues to co-maintain
     // - https://github.com/EmbarkStudios/puffin
     // - https://github.com/EmbarkStudios/poll-promise
     "emilk",
+    // Keith (https://github.com/keith) is contributor-to and co-maintainer of the k8s-buildkite-plugin
+    // https://github.com/EmbarkStudios/k8s-buildkite-plugin
+    "keith",
 ];
 
 /// Some project might be public but not quite ready to be listed on the website
