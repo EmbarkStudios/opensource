5c15fab585142c33768b3e8add6b8896f600c645
Tom Solberg <tom.solberg@embark-studios.com>
2022-05-03 11:30:30 +0200

    Update array size

--- src/policy.rs
+++ src/policy.rs
@@ -2,7 +2,7 @@
 
 /// Generally we require that all maintainers of Embark Studios open source projects are
 /// part of the Embark org, but this list allows some explicit exceptions
-pub const ALLOWED_NON_EMBARK_MAINTAINERS: [&str; 1] = [
+pub const ALLOWED_NON_EMBARK_MAINTAINERS: [&str; 2] = [
     // Emil (https://github.com/emilk) worked at Embark and built 2 open source crates that he continues to co-maintain
     // - https://github.com/EmbarkStudios/puffin
     // - https://github.com/EmbarkStudios/poll-promise
